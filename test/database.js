f830f45e76cfc020b199d9e8285bc34e7081383b
ezirmusitua <jferroal@gmail.com>
2017-10-17 23:56:20 +0800

    Working on simple db
    1. Add test
    2. Add lib
    
    Took 10 minutes

---
+++ test/database.js
@@ -0,0 +1,26 @@
+const { describe, it, xit } = require('mocha');
+const expect = require('chai').expect;
+
+describe('JieBaParser', () => {
+    describe('parse', () => {
+        it('should parse correctly', () => {
+            expect(false).to.equal(true);
+        });
+    });
+});
+
+describe('IndexGenerator', () => {
+    describe('organize', () => {
+        it('should organize parsed as i want', () => {
+            expect(false).to.equal(true);
+        });
+    });
+});
+
+describe('Executor', () => {
+    describe('query', () => {
+        it('should query correctly', () => {
+            expect(false).to.equal(true);
+        });
+    });
+});

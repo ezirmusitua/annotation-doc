902f7fb59a329492a1626b58cced619712f093d6
ezirmusitua <jferroal@gmail.com>
2017-10-18 01:09:32 +0800

    Add Content Parser and tests
    Took 1 hour 4 minutes

--- test/database.js
+++ test/database.js
@@ -1,10 +1,22 @@
 const { describe, it, xit } = require('mocha');
 const expect = require('chai').expect;
+const { ContentParser } = require('../lib/database');
 
 describe('JieBaParser', () => {
+    describe('lint', () => {
+        it('should remove symbols and insert space correctly', () => {
+            const sentence = '我是拖拉机学院手扶拖拉机专业的. 不用多久, 我就会升职加薪, 当上CEO, 走上人生巅峰.';
+            expect(ContentParser._lint(sentence)).to
+              .equal('我是拖拉机学院手扶拖拉机专业的  不用多久  我就会升职加薪  当上 CEO  走上人生巅峰 ')
+        });
+    });
     describe('parse', () => {
         it('should parse correctly', () => {
-            expect(false).to.equal(true);
+            const sentence = '我是拖拉机学院手扶拖拉机专业的. 不用多久, 我就会升职加薪, 当上 CEO, 走上人生巅峰.';
+            expect(ContentParser.parse(sentence)).to.deep.equal([
+                '我', '是', '拖拉', '拖拉机', '学院', '手扶', '手扶拖拉机', '专业', '的', '不用', '多久', '就', '会', '升职',
+                '加薪', '当', '上', 'CEO', '走上', '人生', '巅峰',
+            ])
         });
     });
 });
